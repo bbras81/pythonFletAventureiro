dfc3a972459571e247947c65470d93b202fdeb5c
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-02-28 15:16:05 +0000

    Primeiros componentes

--- aula.py
+++ aula.py
@@ -1,6 +1,17 @@
 import flet as ft 
 
 def main(page: ft.Page):
-    ...
+    page.bgcolor = 'amber'
+    page.bgcolor = ft.colors.AMBER_500
     
-    ft.app(target=main) 
+    page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
+    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    
+    page.add(
+        ft.Text(value="Olá Mundo "),
+        ft.Container(ft.Text(value="Ola Mundo!"),bgcolor='black')
+    )
+    
+    page.update()
+    
+ft.app(target=main)
