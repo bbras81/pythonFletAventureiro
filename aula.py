90a0d822016825eb03b8d681b5e88259ebe55f2c
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-02-29 13:25:58 +0000

    Aula de Page II

--- aula.py
+++ aula.py
@@ -1,7 +1,51 @@
 import flet as ft 
 
 def main(page: ft.Page):
+    
     page.title = 'Flet App'
+
+    # #A janela fica sempre por cima das outras paginas
+    # page.window_always_on_top = False
+
+    # # #Remove a barra superior do sistema operativo
+    # # page.window_title_bar_hidden = False
+    # # #Remove os botões da barra do sistema
+    # # page.window_frameless = False
+    # # #Abre a aplicação em FullScreen
+    # # page.window_full_screen = False
+
+    # #definir o tamanho que abre a tela
+    # page.window_height = 300
+    # # page.window_max_height = 900
+    # page.window_width = 600
+
+    # page.window_resizable = False
+
+    # page.window_top = 50
+    # page.window_left = 20
+    # page.window_movable = True
+    # page.window_prevent_close = False
+    # page.window_progress_bar = 0.5
+
+    # print(page.platform)
+    def page_resize(e):
+        print('Tamanho :', page.window_width, page.window_height)
+
+    def window_event(e):
+        match e.data:
+            case 'moved':
+                print('Moveu a pagina')
+            case 'resized':
+                print('Redimensionoua pagina')
+            case 'minimize':
+                print('Minimizou')
+            case _:
+                print('Outra Opção')
+
+    page.on_resize = page_resize
+
+    page.on_event = window_event
+
 
     page.bgcolor = 'purple'
     page.bgcolor = '#f0fafa'
