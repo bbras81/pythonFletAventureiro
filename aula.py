46d103f5bb28b138f74150948a8b265c599b5496
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-02-29 12:28:59 +0000

    Primeiras Aulas PAge

--- aula.py
+++ aula.py
@@ -1,14 +1,13 @@
 import flet as ft 
 
 def main(page: ft.Page):
-    page.bgcolor = 'amber'
-    page.bgcolor = ft.colors.AMBER_500
+    page.bgcolor = 'green'
     
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     page.vertical_alignment = ft.MainAxisAlignment.CENTER
     
     page.add(
-        ft.Text(value="Olá Mundo "),
+        ft.Text(value="Olá Mundo, Lindo"),
         ft.Container(ft.Text(value="Ola Mundo!"),bgcolor='black')
     )
     
