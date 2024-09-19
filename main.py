5839b3c731175323f8d561a59a746d074f573f68
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-09-19 13:47:12 +0100

    Auto-commit: 2024-09-19 13:47:12

--- main.py
+++ main.py
@@ -3,7 +3,9 @@ def main(page: ft.Page):
     grid = ft. GridView(
         controls=[
             ft.Image(src=f'https://picsum.photos/250/300?{num}', fit='cover') for num in range(20)
-        ]
+        ],
+        runs_count=4,
+        padding=36,
     )
 
     page.add(grid)
