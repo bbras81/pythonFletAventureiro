db607e0c3627243e7fb403ad2dd0583446e2e67b
Bruno Miguel Brás <miguelazevedodg@gmail.com>
2024-09-19 13:14:21 +0100

    Auto-commit: 2024-09-19 13:14:21

---
+++ grid_view.py
@@ -0,0 +1,12 @@
+import flet as ft
+def main(page: ft.Page):
+    grid = ft. GridView(
+        controls=[
+            ft.Image(src=f'https://picsum.photos/250/300?{num}', fit='cover') for num in range(20)
+        ]
+    )
+
+    page.add(grid)
+
+if __name__ =='__main__':
+    ft.app(target = main)
